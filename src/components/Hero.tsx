3909ecc11b040b227bfff60a36f32f60e37ee388
David Onifade <davidicfola@gmail.com>
2024-02-07 00:19:00 +0100

    update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -26,7 +26,7 @@ const Hero = () => {
             }}
           />
         </div>
-        <h1 className="text-5xl md:text-6xl font-extrabold my-3">
+        <h1 className="text-4xl md:text-6xl font-extrabold my-3">
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary animate-gradient">
             David Onifade.
           </span>
