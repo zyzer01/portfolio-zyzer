8489381aa24b53365107a0c3d7b9ab5fb79f2e83
David Onifade <davidicfola@gmail.com>
2024-06-15 16:53:23 +0100

    project live link

--- src/utils/items.tsx
+++ src/utils/items.tsx
@@ -140,7 +140,7 @@ export const projects = [
     description:
       "Lubesurgeons is a platform that provides convenience with its on-demand automobile repair service. You can get swift and expert car care services delivered to your location. Enjoy transparent pricing, skilled technicians, and a seamless user experience, all at your fingertips.",
     repository: "https://github.com/zyzer01/lubesurgeons",
-    liveLink: "www.lubesurgeons.com",
+    liveLink: "https://www.lubesurgeons.com",
     isLive: true,
     isOnGithub: true,
     technologies:
