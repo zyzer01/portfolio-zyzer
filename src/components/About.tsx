3782dc97f5c20dc22f70c9fdf102ab08c3a71732
David Onifade <davidicfola@gmail.com>
2024-07-31 20:19:42 +0100

    update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,8 @@
-import Link from 'next/link';
-import Heading from './ui/Heading';
-import SectionHeading from './ui/SectionHeading';
-import zyzer from '/public/images/portrait.jpg';
-import Image from 'next/image';
+import Link from "next/link";
+import Heading from "./ui/Heading";
+import SectionHeading from "./ui/SectionHeading";
+import zyzer from "/public/images/portrait.jpg";
+import Image from "next/image";
 
 const About = () => {
   return (
@@ -11,34 +11,36 @@ const About = () => {
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-16">
         <div className="text-shade ">
           <p className="leading-relaxed">
-            Hi, there! My name is David, I'm an experienced software developer with 3+ years in
-            standard application development, currently building an enterprise application (news
-            soon), I'm proficient in a number of programming languages and tech tools.
+            Hi, there! My name is David, I'm an experienced software developer
+            with 3+ years in standard application development. I'm proficient in
+            a number of programming languages and tech tools.
           </p>
           <br />
           <p>
-            I'm a strong problem solver and collaborative team player, Over my few years in tech, I
-            have contributed to some amazing open-source projects like{' '}
+            I'm a strong problem solver and collaborative team player, Over my
+            few years in tech, I have contributed to some amazing open-source
+            projects like{" "}
             <span className="transition-all ease duration-300 bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary">
               <Link href="https://github.com/build-trust" target="_blank">
                 Ockam
               </Link>
-            </span>{' '}
-            and{' '}
+            </span>{" "}
+            and{" "}
             <span className="transition-all ease duration-300 bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary">
               <Link href="https://github.com/devflowinc/trieve" target="_blank">
                 Arguflow
               </Link>
-            </span>{' '}
-            actively engaging with the developer community to foster innovation and drive industry
-            advancement. I've also ran a web design agency, where I and my partner, Kolawole built
-            and delivered over 20 web apps to our clients all over the world. I'm Passionate about
+            </span>{" "}
+            actively engaging with the developer community to foster innovation
+            and drive industry advancement. I've also ran a web design agency,
+            where I and my partner, Kolawole built and delivered over 20 web
+            apps to our clients all over the world. I'm Passionate about
             leveraging technology to create impactful solutions.
           </p>
           <br />
           <p>
-            I thrive in dynamic environments where I can apply my skills to solve problems and drive
-            meaningful outcomes.
+            I thrive in dynamic environments where I can apply my skills to
+            solve problems and drive meaningful outcomes.
           </p>
         </div>
         <div className="group">
